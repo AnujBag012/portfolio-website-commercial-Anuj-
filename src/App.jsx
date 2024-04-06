499a92ac4a13db65cb4570b782ede6a17f0c7270
Pranav-Kumar-Singh-073 <kumarharsh073@gmail.com>
2024-04-06 12:02:15 +0530

    resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { RecoilRoot } from 'recoil'
 import Slider from './componets-s/Slider'
 import { Footer } from './componets-s/Footer'
 import React, { useRef, useState } from 'react';
-import {RecoilRoot} from 'recoil';
-import {Gallery} from './components/Gallery'
+import { Gallery } from './components/Gallery'
 import { Navbar } from './components/Navbar';
 
 
@@ -11,16 +10,11 @@ export default function App() {
   return (
     <div>
       <RecoilRoot>
-        {/* <Slider/> */}
-        <Footer/>
+        {/* <Slider/>
+        <Footer/> */}
+        {/* <Gallery></Gallery> */}
+        <Navbar></Navbar>
       </RecoilRoot>
     </div>
   )
-      <div>
-        <RecoilRoot>
-          {/* <Gallery></Gallery> */}
-          {/* <Navbar></Navbar> */}
-        </RecoilRoot>
-      </div>
-  );
 }
