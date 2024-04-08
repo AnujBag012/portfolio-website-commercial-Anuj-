8800f443bb7eacccf9203a30a34f2f0fd1f623b6
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-04-08 18:23:52 +0530

    adjusted the size of gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,8 +6,8 @@ export function Gallery() {
         <div className="grid justify-items-center" id='gallery'>
         <div className="flex items-center flex-col w-full">
             <div className="flex-1 border-t border-gray-400"></div>
-            <div className="px-4 text-4xl">Gallery</div>
-            <div className="py-4 text-gray-700">Cherishing Moments</div>
+            <div className="px-4 text-5xl">Gallery</div>
+            <div className="py-4 text-gray-700 text-xl">Cherishing Moments</div>
             <div className='pb-96'><Swipe/></div>
         </div>
        
